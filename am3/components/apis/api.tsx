c7a6ecf995d91c72d8fa64602dc0bf832e1b8e55
CA-JunPark <cskoko5786@gmail.com>
2025-02-22 17:30:35 -0800

    test build

--- am3/components/apis/api.tsx
+++ am3/components/apis/api.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
-import { baseURL, refreshTokenUrl } from '@/assets/fixedData/env';
+import { baseURL, refreshTokenUrl } from './apiUrls';
 import { Alert } from 'react-native';
 
 const api = axios.create({
